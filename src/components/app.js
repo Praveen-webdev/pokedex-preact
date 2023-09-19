85c11c7bfaf8f4eaa95431fcba05b1d00d94202a
Praveen <praveenofcl07@gmail.com>
2023-09-19 10:04:09 +0530

    router event

--- src/components/app.js
+++ src/components/app.js
@@ -5,26 +5,19 @@ import NotFound from "../routes/not-found";
 import Pokemons from "../routes/pokemons";
 import Pokemon from "../routes/pokemon";
 import PokemonProvider from "../components/pokemon-provider";
-import CaughtPokemons from "../routes/my-pokemons";
+import MyPokemons from "../routes/my-pokemons";
 
-const handleRouteChange = (e) => {
-  if (e.previous === "/pokemons") {
-    const pokemons = localStorage.getItem('pokemons');
-    if (pokemons) {
-      localStorage.setItem('pokemons', pokemons);
-    }
-  }
-};
+
 
 const App = () => (
   <div id="app">
     <PokemonProvider>
       <Header />
       <main class="wrapper">
-        <Router onChange={handleRouteChange}>
+        <Router onChange={(e)=>console.log("changing route",e)}>
           <Pokemons path="/" />
           <Pokemon path="/pokemon/:id" />
-          <CaughtPokemons path="/my-pokemons" />
+          <MyPokemons path="/my-pokemons" />
           <NotFound default />
         </Router>
       </main>
